d9dee95efdba37925d4f43a72d653ff4f02cf37e
Danilo Andrusco <desahogo.cl@gmail.com>
2023-10-11 08:59:57 -0300

    agregue la division

---
+++ Division.py
@@ -0,0 +1,12 @@
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: división por cero"
+    
+print("4. División")
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+print("Resultado:", division(num1, num2))
