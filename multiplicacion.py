b776e8487d919d5679e1fe23028bfbbc80d91364
Danilo Andrusco <desahogo.cl@gmail.com>
2023-11-15 12:04:59 -0300

    modifico la multiplicacion

--- multiplicacion.py
+++ multiplicacion.py
@@ -1,7 +1,7 @@
 def multiplicacion(a, b):
     return a * b
 
-print("3. Multiplicación")
+print("3. Multiplicaciónes")
 
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
