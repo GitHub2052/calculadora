ba8c0a00b34757f36808fe4917977ac8e84dedee
dAndrusco <dandrusco@gmail.com>
2024-05-30 08:31:14 -0400

    Modificamos la division

--- Division.py
+++ Division.py
@@ -4,7 +4,7 @@ def division(a, b):
     else:
         return "Error: división por cero"
     
-print("4. División")
+print("4. Divisiónes")
 
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
